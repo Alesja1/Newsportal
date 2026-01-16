28fcf01701ee36d2a68c727eef5031785699059c
Alesja Avdejeva <alesja.avdejeva@ivkhk.ee>
2026-01-16 19:34:18 +0200

    glavnaja stranitsa

---
+++ route/routing.php
@@ -0,0 +1,34 @@
+<?php
+//вычислить маршрут из адресной строки
+    $host = explode('?', $_SERVER['REQUEST_URI'])[0];
+    $num = substr_count($host, '/');
+    $path = explode('/', $host)[$num];
+
+    //echo $host;
+    //echo $num;
+    //echo $path;
+    //die;
+
+    //echo 'rrrr';
+    echo $path;
+    if ($path == '' OR $path == 'index' OR $path == 'index.php') { 
+        //echo 'test';
+        $response = Controller::StartSite(); 
+    }
+
+    elseif ($path == 'all') { 
+        $response = Controller::AllNews(); 
+    }
+    elseif ($path == 'category' and isset($_GET['id'])) { 
+        $response = Controller::NewsByCatID($_GET['id']);
+    }
+    elseif ($path == 'news' and isset($_GET['id'])) { 
+        $response = Controller::NewsByID($_GET['id']); 
+    }
+
+    else{ 
+            $response = Controller::error404();
+    }
+
+
+?> 
