c7231c702cea927ec3d1715eda82824802555260
Alesja Avdejeva <alesja.avdejeva@ivkhk.ee>
2026-01-19 19:40:33 +0200

    rabotajut kommentarii

--- view/readnews.php
+++ view/readnews.php
@@ -1,13 +1,17 @@
 <?php
 ob_start();
 ?>
-
 <br>
-
 <?php
-ViewNews::ReadNews($n);
+ViewNews::ReadNews($n); 
+
+echo "<br>";
+Controller::Comments($_GET['id']);
+
+echo "<br>";
+ViewComments::CommentsForm(); 
 
 $content = ob_get_clean();
 include_once 'view/layout.php';
-
 ?>
+
